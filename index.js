bc8d21a326b3a7477cfcd3bd87c93b0c2b6f5021
Md Mehedi Hasan <116740305+engmehedihasan@users.noreply.github.com>
2022-11-15 11:03:55 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const fs = require("fs")
+const PORT = 3000;
+
+const hostName = '127.0.0.1';
+
+const server = http.createServer((req, res) =>{
+    
+    fs.readFile("index.html",(err,data)=>{
+        res.writeHead(200,{"Content-Type": "text/html"});
+        res.write(data);
+        res.end();
+    })
+
+});
+server.listen(PORT, hostName,()=>{
+    console.log('Server is running Successfully  at http://${hostName}:${PORT}');
+});
